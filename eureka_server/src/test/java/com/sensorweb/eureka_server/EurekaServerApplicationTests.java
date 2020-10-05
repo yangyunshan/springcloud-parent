44aa0f325ab7df1d52c3db7d2e1134966dbda223
yangyunshan <yangyunshan123@gmail.com>
2020-10-05 18:04:45 +0800

    Initial commit

---
+++ eureka_server/src/test/java/com/sensorweb/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensorweb.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
